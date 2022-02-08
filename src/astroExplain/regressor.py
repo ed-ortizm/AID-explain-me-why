9ea27471c3171b3f6f58e7e4837b2646d9a646c0
Edgar Ortiz <ed.ortizm@gmail.com>
2022-02-08 17:13:39 -0300

    Set layout for silly regressor that works on images [no need to train ;)]

---
+++ src/astroExplain/regressor.py
@@ -0,0 +1,10 @@
+import numpy as np
+class GalaxyPlus:
+    """
+    Class to add all pixel values in an image
+    """
+    def __init__(self):
+        pass
+
+    def predict(self, image: np.array)->float:
+        pass
