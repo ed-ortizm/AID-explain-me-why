ff9f6a9c9c93304ce994de36b19690af6c803bcb
Edgar Ortiz <ed.ortiz@gmail.com>
2022-02-09 09:47:43 -0300

    Coment lines that inspect the explanantion

--- imageExplain.py
+++ imageExplain.py
@@ -25,7 +25,10 @@ print("Explain GalaxyPlus model", end="\n")
 # Load model
 addGalaxy = GalaxyPlus()
 # Set explainer instance
-explainer = lime_image.LimeImageExplainer(feature_selection="'highest_weights'")
+explainer = lime_image.LimeImageExplainer(
+    kernel_width=0.25, # default is .25
+    feature_selection="highest_weights"
+)
 # get explanation
 explanation = explainer.explain_instance(
     image=galaxy,
@@ -36,59 +39,59 @@ explanation = explainer.explain_instance(
     num_samples=1000
 )
 ###############################################################################
-print("Inpect explanation", end="\n")
-save_to = parser.get("directory", "output")
-# save image with positive contributing meaningful features
-temp, mask = explanation.get_image_and_mask(
-    # explanation.top_labels[0],
-    positive_only=True,
-    num_features=5,
-    hide_rest=True
-)
-
-galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
-np.save(f"{save_to}/{name_galaxy}_positive.npy", galaxyExplanation)
-###########################################################
-# save whole image with boundaries
-temp, mask = explanation.get_image_and_mask(
-    # explanation.top_labels[0],
-    positive_only=True,
-    num_features=5,
-    hide_rest=False
-)
-
-galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
-np.save(f"{save_to}/{name_galaxy}_positiveBounded.npy", galaxyExplanation)
-###########################################################
-# save represantions ilustrating possitive and detrimental
-# menainful features
-
-temp, mask = explanation.get_image_and_mask(
-    # explanation.top_labels[0],
-    positive_only=True,
-    num_features=5,
-    hide_rest=False
-)
-
-galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
-np.save(f"{save_to}/{name_galaxy}_contribute.npy", galaxyExplanation)
-###############################################################################
-# heatmap visualization
-# code from lime repo
-print("Get heat map visualization", end="\n")
-
-#Select the same class explained on the figures above.
-ind =  explanation.top_labels[0]
-
-#Map each explanation weight to the corresponding superpixel
-dict_heatmap = dict(explanation.local_exp[ind])
-heatmap = np.vectorize(dict_heatmap.get)(explanation.segments)
-
-#Plot. The visualization makes more sense if a symmetrical colorbar is used.
-plt.imshow(heatmap, cmap = 'RdBu', vmin  = -heatmap.max(), vmax = heatmap.max())
-plt.colorbar()
-
-plt.savefig(f"{save_to}/{name_galaxy}_heatMap.png")
+# print("Inpect explanation", end="\n")
+# save_to = parser.get("directory", "output")
+# # save image with positive contributing meaningful features
+# temp, mask = explanation.get_image_and_mask(
+#     # explanation.top_labels[0],
+#     positive_only=True,
+#     num_features=5,
+#     hide_rest=True
+# )
+#
+# galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
+# np.save(f"{save_to}/{name_galaxy}_positive.npy", galaxyExplanation)
+# ###########################################################
+# # save whole image with boundaries
+# temp, mask = explanation.get_image_and_mask(
+#     # explanation.top_labels[0],
+#     positive_only=True,
+#     num_features=5,
+#     hide_rest=False
+# )
+#
+# galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
+# np.save(f"{save_to}/{name_galaxy}_positiveBounded.npy", galaxyExplanation)
+# ###########################################################
+# # save represantions ilustrating possitive and detrimental
+# # menainful features
+#
+# temp, mask = explanation.get_image_and_mask(
+#     # explanation.top_labels[0],
+#     positive_only=True,
+#     num_features=5,
+#     hide_rest=False
+# )
+#
+# galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
+# np.save(f"{save_to}/{name_galaxy}_contribute.npy", galaxyExplanation)
+# ###############################################################################
+# # heatmap visualization
+# # code from lime repo
+# print("Get heat map visualization", end="\n")
+#
+# #Select the same class explained on the figures above.
+# ind =  explanation.top_labels[0]
+#
+# #Map each explanation weight to the corresponding superpixel
+# dict_heatmap = dict(explanation.local_exp[ind])
+# heatmap = np.vectorize(dict_heatmap.get)(explanation.segments)
+#
+# #Plot. The visualization makes more sense if a symmetrical colorbar is used.
+# plt.imshow(heatmap, cmap = 'RdBu', vmin  = -heatmap.max(), vmax = heatmap.max())
+# plt.colorbar()
+#
+# plt.savefig(f"{save_to}/{name_galaxy}_heatMap.png")
 ###############################################################################
 finish_time = time.time()
 print(f"Run time: {finish_time-start_time:.2f}")
