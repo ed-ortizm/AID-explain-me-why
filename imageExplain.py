a1be605190223f7c31992454d0342f409a5f36ee
Edgar Ortiz <ed.ortizm@gmail.com>
2022-02-08 21:52:29 -0300

    Add code to save data of explanations with different configurations of meaninful features. Save heatmap of explanations

--- imageExplain.py
+++ imageExplain.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 from lime import lime_image
 import numpy as np
+from skimage.segmentation import mark_boundaries
 
 from astroExplain.regressor import GalaxyPlus
 ###############################################################################
@@ -22,10 +23,71 @@ galaxy = np.array(Image.open("data/NGC3432.jpg")).astype(float)
 ###############################################################################
 print("Explain GalaxyPlus model", end="\n")
 # Load model
-galaxyAdd = GalaxyPlus()
-print(galaxyAdd.predict(galaxy))
+addGalaxy = GalaxyPlus()
 # Set explainer instance
 explainer = lime_image.LimeImageExplainer()
+# get explanation
+explanation = explainer.explain_instance(
+    image=galaxy,
+    classifier_fn=addGalaxy.predict,
+    # top_labels=5,
+    hide_color=0,
+    num_samples=100
+)
+###############################################################################
+print("Inpect explanation", end="\n")
+save_to = parser.get("directory", "output")
+# save image with positive contributing meaningful features
+temp, mask = explanation.get_image_and_mask(
+    # explanation.top_labels[0],
+    positive_only=True,
+    num_features=5,
+    hide_rest=True
+)
+
+galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
+np.save(f"{save_to}/{name_galaxy}_positive.npy", galaxyExplanation)
+###########################################################
+# save whole image with boundaries
+temp, mask = explanation.get_image_and_mask(
+    # explanation.top_labels[0],
+    positive_only=True,
+    num_features=5,
+    hide_rest=False
+)
+
+galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
+np.save(f"{save_to}/{name_galaxy}_positiveBounded.npy", galaxyExplanation)
+###########################################################
+# save represantions ilustrating possitive and detrimental
+# menainful features
+
+temp, mask = explanation.get_image_and_mask(
+    # explanation.top_labels[0],
+    positive_only=True,
+    num_features=5,
+    hide_rest=False
+)
+
+galaxyExplanation = mark_boundaries(temp / 2 + 0.5, mask)
+np.save(f"{save_to}/{name_galaxy}_contribute.npy", galaxyExplanation)
+###############################################################################
+# heatmap visualization
+# code from lime repo
+print("Get heat map visualization", end="\n")
+
+#Select the same class explained on the figures above.
+ind =  explanation.top_labels[0]
+
+#Map each explanation weight to the corresponding superpixel
+dict_heatmap = dict(explanation.local_exp[ind])
+heatmap = np.vectorize(dict_heatmap.get)(explanation.segments)
+
+#Plot. The visualization makes more sense if a symmetrical colorbar is used.
+plt.imshow(heatmap, cmap = 'RdBu', vmin  = -heatmap.max(), vmax = heatmap.max())
+plt.colorbar()
+
+plt.savefig(f"{save_to}/{name_galaxy}_heatMap.png")
 ###############################################################################
 finish_time = time.time()
 print(f"Run time: {finish_time-start_time:.2f}")
