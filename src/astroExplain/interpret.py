cf07825f8e4d0110c75c1db07f9e6c6405c8ae03
Edgar Ortiz <ed.ortizm@gmail.com>
2022-02-10 16:46:41 -0300

    Implement show_me and heat_map methods

--- src/astroExplain/interpret.py
+++ src/astroExplain/interpret.py
@@ -13,23 +13,28 @@ class TellMeWhy:
     ###########################################################################
     def show_me(
         self,
-        positive_only: bool,
-        negative_only: bool,
-        number_of_features: int,
-        hide_rest: bool,
-        min_weight: float,
+        positive_only: bool=True,
+        negative_only: bool=False,
+        number_of_features: int=5,
+        hide_rest: bool=False,
+        minimum_weight: float=-np.inf,
+        show_explanation: bool=False,
     ) -> np.array:
 
         image, mask = self.explanation.get_image_and_mask(
-            label=explanation.top_labels[0],
+            label=self.explanation.top_labels[0],
             positive_only=positive_only,
             negative_only=negative_only,
-            num_features=num_features,
+            num_features=number_of_features,
             hide_rest=hide_rest,
-            min_weight = min_weight
+            min_weight = minimum_weight
         )
 
-        visual_explanation = mark_boundaries(temp, mask)
+        visual_explanation = mark_boundaries(image, mask)
+
+        if show_explanation is True:
+
+            plt.imshow(visual_explanation)
 
         return visual_explanation
     ###########################################################################
@@ -45,8 +50,8 @@ class TellMeWhy:
         ind = self.explanation.top_labels[0]
 
         # Map each explanation weight to the corresponding superpixel
-        dict_heatmap = dict(explanation.local_exp[ind])
-        heatmap = np.vectorize(dict_heatmap.get)(explanation.segments)
+        dict_heatmap = dict(self.explanation.local_exp[ind])
+        heatmap = np.vectorize(dict_heatmap.get)(self.segments)
 
         #The visualization makes more sense if a symmetrical colorbar is used.
         plt.imshow(
@@ -62,5 +67,4 @@ class TellMeWhy:
 
             plt.savefig(f"{save_to}/{galaxy_name}HeatMapExp.{save_format}")
 
-        plt.close()
 ###############################################################################
