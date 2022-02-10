071b6f78eb57b85f8dff6fe423612a57a6d00f9f
Edgar Ortiz <ed.ortizm@gmail.com>
2022-02-10 16:11:13 -0300

    Set show_me and heatmap methods of TellMeWhy class

--- src/astroExplain/interpret.py
+++ src/astroExplain/interpret.py
@@ -1,47 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from skimage.segmentation import mark_boundaries
+from lime.lime_image import ImageExplanation
 ###############################################################################
 class TellMeWhy:
-    def __init__(self):
-        pass
+    def __init__(self, explanation: ImageExplanation):
 
-    def show_me(self):
-        # save image with positive contributing meaningful features
-        temp, mask = explanation.get_image_and_mask(
+        self.explanation = explanation
+        self.galaxy = explanation.image
+        self.segments = explanation.segments
+
+    ###########################################################################
+    def show_me(
+        self,
+        positive_only: bool,
+        negative_only: bool,
+        number_of_features: int,
+        hide_rest: bool,
+        min_weight: float,
+    ) -> np.array:
+
+        image, mask = self.explanation.get_image_and_mask(
             label=explanation.top_labels[0],
-            positive_only=False,
-            num_features=100,
-            hide_rest=False,
-            # min_weight = -100
+            positive_only=positive_only,
+            negative_only=negative_only,
+            num_features=num_features,
+            hide_rest=hide_rest,
+            min_weight = min_weight
         )
 
-        galaxyExplanation = mark_boundaries(temp, mask)
-        # import matplotlib.pyplot as plt
-        # plt.imshow(galaxyExplanation)
-        # plt.show()
+        visual_explanation = mark_boundaries(temp, mask)
 
-        ###############################################################################
-        save_to = parser.get("directory", "output")
-        if os.path.exists(save_to) is False:
-            os.mkdir(save_to)
-        np.save(f"{save_to}/{name_galaxy}_allExp.npy", galaxyExplanation)
-        pass
+        return visual_explanation
+    ###########################################################################
+    def heatmap(self,
+        show_map: bool=False,
+        save_map: bool=False,
+        save_to: str=".", galaxy_name: str="name", save_format: str=".png"
+    ) -> None:
 
-    def heatmap(self):
-        # heatmap visualization
-        # code from lime repo
-        print("Get heat map visualization", end="\n")
+        print("Get heat map of explanation", end="\n")
 
-        # Select the same class explained on the figures above.
-        ind = explanation.top_labels[0]
+        # there is only one label, since I have a regressor
+        ind = self.explanation.top_labels[0]
 
         # Map each explanation weight to the corresponding superpixel
         dict_heatmap = dict(explanation.local_exp[ind])
         heatmap = np.vectorize(dict_heatmap.get)(explanation.segments)
 
-        # Plot. The visualization makes more sense if a symmetrical colorbar is used.
-        plt.imshow(heatmap, cmap="RdBu", vmin=-heatmap.max(), vmax=heatmap.max())
+        #The visualization makes more sense if a symmetrical colorbar is used.
+        plt.imshow(
+            heatmap, cmap="RdBu", vmin=-heatmap.max(), vmax=heatmap.max()
+        )
+
         plt.colorbar()
-        # plt.show()
-        plt.savefig(f"{save_to}/{name_galaxy}_heatMap.png")
-        pass
+
+        if show_map is True:
+            plt.show()
+
+        if save_map is True:
+
+            plt.savefig(f"{save_to}/{galaxy_name}HeatMapExp.{save_format}")
+
+        plt.close()
 ###############################################################################
